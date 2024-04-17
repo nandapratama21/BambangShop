14c67b81dbd2f481b3676a3ad02c709636bb9222
nandapratama21 <nanda241power@gmail.com>
2024-04-17 16:08:55 +0700

    Implement publish function in Program service and Program controller

--- src/controller/product.rs
+++ src/controller/product.rs
@@ -36,4 +36,14 @@ pub fn delete(id: usize) -> Result<Json<Product>> {
         Ok(f) => Ok(Json::from(f)),
         Err(e) => Err(e)
     };
+
+}
+
+
+#[post("/<id>/publish")]
+pub fn publish(id:usize)-> Result<Json<Product>> {
+    return match ProductService::publish(id) {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
+    };
 }
